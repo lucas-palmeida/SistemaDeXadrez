0f3d5049f5cac26e8d447e1845142385d45e9c5f
Lucas Pacheco <lucaspacheco95@gmail.com>
2022-07-05 22:03:45 -0300

    Atualizações classes ChessMatch e Board, criadas BoardException e classes Pieces

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardGame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece{
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+}
