3d66871de54b2c228972c8d4ccd9549e67ddab9b
Lucas Pacheco <lucaspacheco95@gmail.com>
2022-07-08 16:06:17 -0300

    Implementado limpa tela, tratamento de excecoes e movimentos da torre

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,8 +2,9 @@ package chess;
 
 import boardGame.Board;
 import boardGame.Piece;
+import boardGame.Position;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 
 	private Color color;
 	
@@ -14,5 +15,10 @@ public class ChessPiece extends Piece{
 
 	public Color getColor() {
 		return color;
+	}
+	
+	protected boolean isThereAnyOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
 	}
 }
